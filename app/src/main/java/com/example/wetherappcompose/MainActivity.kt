7ec2089fecf1c9fe86231261b8059af03b5a9bea
Denis Ryzhkov <dgryzhkov@gmail.com>
2023-02-08 14:54:18 +0700

    tablayout

--- app/src/main/java/com/example/wetherappcompose/MainActivity.kt
+++ app/src/main/java/com/example/wetherappcompose/MainActivity.kt
@@ -3,7 +3,14 @@ package com.example.wetherappcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.wetherappcompose.ui.screens.MainScreen
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.example.wetherappcompose.ui.screens.MainCard
+import com.example.wetherappcompose.ui.screens.TabLayout
 import com.example.wetherappcompose.ui.theme.WetherAppComposeTheme
 
 const val API_KEY = "7c3a302d7b024735ab554908230302"
@@ -13,7 +20,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WetherAppComposeTheme {
-                MainScreen()
+                Image(
+                    painter = painterResource(id = R.drawable.weather_bg), contentDescription = "im1",
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentScale = ContentScale.FillBounds
+                )
+                Column {
+                    MainCard()
+                    TabLayout()
+
+                }
             }
         }
     }
--- app/src/main/java/com/example/wetherappcompose/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/wetherappcompose/ui/screens/MainScreen.kt
@@ -1,17 +1,15 @@
 package com.example.wetherappcompose.ui.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,22 +18,20 @@ import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.wetherappcompose.R
 import com.example.wetherappcompose.ui.theme.BlueLight
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
 
 /**
  *@Author Dgryzhkov
  */
 @Preview(showBackground = true)
 @Composable
-fun MainScreen() {
-    Image(
-        painter = painterResource(id = R.drawable.weather_bg), contentDescription = "im1",
-        modifier = Modifier
-            .fillMaxSize(),
-        contentScale = ContentScale.FillBounds
-    )
+fun MainCard() {
     Column(
         modifier = Modifier
-            .fillMaxSize()
             .padding(5.dp)
     ) {
         Card(
@@ -59,12 +55,16 @@ fun MainScreen() {
                         style = TextStyle(fontSize = 15.sp),
                         color = Color.Black
                     )
+
                     AsyncImage(
                         model = "https://cdn.weatherapi.com/weather/64x64/night/113.png",
-                        contentDescription = "im2", modifier = Modifier
+                        contentDescription = "im2",
+                        modifier = Modifier
                             .size(45.dp)
-                            .padding(top = 3.dp, end = 8.dp)
-                    )
+                            .padding(top = 3.dp, end = 8.dp),
+
+
+                        )
                 }
                 Text(
                     text = "Tomsk",
@@ -116,6 +116,62 @@ fun MainScreen() {
                 }
             }
 
+        }
+    }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun TabLayout() {
+    val tabList = listOf("HOURS", "DAYS")
+    val pagerState = rememberPagerState()
+    val tabIndex = pagerState.currentPage
+    val coroutineScope = rememberCoroutineScope()
+    Column(
+        modifier = Modifier
+            .padding(
+                start = 5.dp,
+                end = 5.dp
+            )
+            .clip(
+                RoundedCornerShape(5.dp)
+            )
+    ) {
+        TabRow(
+            selectedTabIndex = tabIndex,
+            indicator = { pos ->
+                TabRowDefaults.Indicator(
+                    Modifier.pagerTabIndicatorOffset(pagerState, pos)
+                )
+            },
+            backgroundColor = BlueLight
+        ) {
+            tabList.forEachIndexed { index, text ->
+                Tab(
+                    selected = false,
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
+                    },
+                    text = {
+                        Text(text = text)
+                    }
+                )
+            }
+        }
+        HorizontalPager(
+            count = tabList.size,
+            state = pagerState,
+            modifier = Modifier.weight(1.0f)
+        ) { index ->
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                items(15) {
+                    ListItem()
+                }
+            }
         }
     }
 }
