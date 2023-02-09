952eb1dd45fc4a4198d61168a26dc47671ca126f
Denis Ryzhkov <dgryzhkov@gmail.com>
2023-02-09 15:05:04 +0700

    getData

--- app/src/main/java/com/example/wetherappcompose/MainActivity.kt
+++ app/src/main/java/com/example/wetherappcompose/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.wetherappcompose
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -9,6 +11,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.wetherappcompose.ui.screens.MainCard
 import com.example.wetherappcompose.ui.screens.TabLayout
 import com.example.wetherappcompose.ui.theme.WetherAppComposeTheme
@@ -20,8 +25,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WetherAppComposeTheme {
+                getData("Tomsk", this)
                 Image(
-                    painter = painterResource(id = R.drawable.weather_bg), contentDescription = "im1",
+                    painter = painterResource(id = R.drawable.weather_bg),
+                    contentDescription = "im1",
                     modifier = Modifier
                         .fillMaxSize(),
                     contentScale = ContentScale.FillBounds
@@ -34,4 +41,25 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+private fun getData(city: String, context: Context) {
+    val url = "https://api.weatherapi.com/v1/forecast.json?key=$API_KEY" +
+            "&q=$city" +
+            "&days =" +
+            "3" +
+            "&aqi = no&alerts = no"
+
+    val queue = Volley.newRequestQueue(context)
+    val sRequest = StringRequest(
+        Request.Method.GET,
+        url,
+        { response ->
+            Log.d("Mylog", "Response: $response")
+        },
+        {
+            Log.d("Mylog", "VolleyError: $it")
+        }
+    )
+    queue.add(sRequest)
 }
