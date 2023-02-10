bd7fbcc58a2b73d3920992daef15698a4ad07e12
Denis Ryzhkov <dgryzhkov@gmail.com>
2023-02-10 14:17:14 +0700

    pars list day

--- app/src/main/java/com/example/wetherappcompose/MainActivity.kt
+++ app/src/main/java/com/example/wetherappcompose/MainActivity.kt
@@ -8,15 +8,20 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.example.wetherappcompose.data.WeatherModel
 import com.example.wetherappcompose.ui.screens.MainCard
 import com.example.wetherappcompose.ui.screens.TabLayout
 import com.example.wetherappcompose.ui.theme.WetherAppComposeTheme
+import org.json.JSONObject
 
 const val API_KEY = "7c3a302d7b024735ab554908230302"
 
@@ -25,7 +30,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WetherAppComposeTheme {
-                getData("Tomsk", this)
+                val daysList = remember {
+                    mutableStateOf(listOf<WeatherModel>())
+                }
+                getData("Tomsk", this, daysList)
                 Image(
                     painter = painterResource(id = R.drawable.weather_bg),
                     contentDescription = "im1",
@@ -35,7 +43,7 @@ class MainActivity : ComponentActivity() {
                 )
                 Column {
                     MainCard()
-                    TabLayout()
+                    TabLayout(daysList)
 
                 }
             }
@@ -43,11 +51,11 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-private fun getData(city: String, context: Context) {
+private fun getData(city: String, context: Context, daysList: MutableState<List<WeatherModel>>) {
     val url = "https://api.weatherapi.com/v1/forecast.json?key=$API_KEY" +
             "&q=$city" +
             "&days =" +
-            "3" +
+            "2" +
             "&aqi = no&alerts = no"
 
     val queue = Volley.newRequestQueue(context)
@@ -55,11 +63,41 @@ private fun getData(city: String, context: Context) {
         Request.Method.GET,
         url,
         { response ->
-            Log.d("Mylog", "Response: $response")
+            val list = getWeatherByDays(response)
+            daysList.value = list
         },
         {
             Log.d("Mylog", "VolleyError: $it")
         }
     )
     queue.add(sRequest)
+}
+
+private fun getWeatherByDays(response: String): List<WeatherModel> {
+    if (response.isEmpty()) return listOf()
+    val list = ArrayList<WeatherModel>()
+    val mainObject = JSONObject(response)
+    val city = mainObject.getJSONObject("location").getString("name")
+    val days = mainObject.getJSONObject("forecast").getJSONArray("forecastday")
+
+    for (i in 0 until days.length()) {
+        val item = days[i] as JSONObject
+        list.add(
+            WeatherModel(
+                city,
+                item.getString("date"),
+                "",
+                item.getJSONObject("day").getJSONObject("condition").getString("text"),
+                item.getJSONObject("day").getJSONObject("condition").getString("icon"),
+                item.getJSONObject("day").getString("maxtemp_c"),
+                item.getJSONObject("day").getString("mintemp_c"),
+                item.getJSONArray("hour").toString()
+            )
+        )
+    }
+    list[0] = list[0].copy(
+        time = mainObject.getJSONObject("current").getString("last_updated"),
+        currentTemp = mainObject.getJSONObject("current").getString("temp_c"),
+    )
+    return list
 }
